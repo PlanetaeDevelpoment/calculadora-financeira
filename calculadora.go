2dc2aaf98f7d914f0f726456ee1b3fc77aaa16a1
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-03-02 12:48:13 +0000

    chore: calculadora agora consegue calcular saldo de salário

--- calculadora.go
+++ calculadora.go
@@ -1,6 +1,8 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	baseAviso   = 30
@@ -11,7 +13,22 @@ func Date(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
 
-func SaldoSalario(salario float64, dataDemissao time.Time) (saldo float64) {
-	dataAposAviso := dataDemissao.AddDate(0, 0, baseAviso+3)
-	return salario * (float64(dataAposAviso.Day()) / baseSalario)
+func SaldoSalario(salario float64, dataContratação, dataDemissao time.Time) (saldo float64) {
+	porcentagemSaldo := getPorcentagemSaldo(dataContratação, dataDemissao)
+	return salario * porcentagemSaldo
+}
+
+func getPorcentagemSaldo(dataContratação, dataDemissao time.Time) float64 {
+	tempoDeAviso := getTempoDeAviso(dataContratação, dataDemissao)
+	dataAposAviso := dataDemissao.AddDate(0, 0, tempoDeAviso)
+	return (float64(dataAposAviso.Day()) / baseSalario)
+}
+
+func getTempoDeAviso(dataContratação, dataDemissao time.Time) int {
+	anosContribuição := getAnosContribuição(dataContratação, dataDemissao)
+	return baseAviso + (3 * anosContribuição)
+}
+
+func getAnosContribuição(inicio, fim time.Time) int {
+	return int(fim.Sub(inicio).Hours() / (24 * 365))
 }
