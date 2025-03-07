99aa8dddbce227502da1e7600ed2be8bce96aa2a
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-03-07 08:25:40 +0000

    chore: create request handler

--- calculadora-recisao/main.go
+++ calculadora-recisao/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
+	"fmt"
+
+	"github.com/EdmilsonRodrigues/calculadora-financeira/calculadora-recisao/calculadora"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
@@ -11,10 +15,18 @@ func main() {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var requisicao calculadorarecisao.RequisiçãoRecisão
+	err := json.Unmarshal([]byte(request.Body), &requisicao)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid JSON payload",
+			StatusCode: 400,
+		}, nil
+	}
+
+	result := calculadorarecisao.CalcularRecisão(requisicao)
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello World!",
+		Body:       fmt.Sprintf("%v", result),
 		StatusCode: 200,
 	}, nil
 }
-
-
