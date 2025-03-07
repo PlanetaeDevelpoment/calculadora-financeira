bec29bdf961ae60d44927f36874f72966e9c1e52
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-03-07 08:20:22 +0000

    feat: start to integrate with AWS Lambda

--- calculadora-recisao/main.go
+++ calculadora-recisao/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello World!",
+		StatusCode: 200,
+	}, nil
+}
+
+
